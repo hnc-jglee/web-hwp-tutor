6fce8ee3b569cbf6d2c835be40f328e29dc4cac9
Jonggyu Lee <jglee1027@gmail.com>
2021-08-24 01:12:03 +0900

    WebHwp Tutor 프레임 초기 구현
    * WebHwp Tutor 메뉴바 구현
    * react-iframe을 이용하여 WebIDE와 웹한글기안기 렌더링 구현
    * App.css xxx-xxx 스타일로 변경

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Getting Started with Create React App
+# WebHwp Tutor App
+웹한글 기안기 튜토리얼 App
+
+## Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Iframe from 'react-iframe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="menu-bar">
+      <p>WebHwp Tutor</p>
+      <div className="app-workspace">
+        <div className="app-workspace-item">
+          <Iframe url="http://webhw-webid-1m5mfhjm0oi2w-560475011.ap-northeast-2.elb.amazonaws.com/#/home/project" width="100%" height="100%" />
+        </div>
+        <div className="app-workspace-item">
+          <Iframe url="http://webhw-webhw-nqgw1bnwi4e8-1170506925.ap-northeast-2.elb.amazonaws.com/webhwpctrl" width="100%" height="100%" />
+        </div>
+      </div>
     </div>
   );
 }
